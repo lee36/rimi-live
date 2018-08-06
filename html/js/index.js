727d5f2bb3f054851061a51347342f1a7154ef6e
kobatospony <1213028676@qq.com>
2018-08-06 21:55:37 +0800

    index page

--- html/js/index.js
+++ html/js/index.js
@@ -1,4 +1,9 @@
 $(function () {
+    // 绑定事件
+    $('#logout_btn').on('click',logout);
+    // 判断是否登录
+    check_user_display();
+    // 显示提示消息
     $('#login_msg').css('display','none');
    // 登录
     $('#login_btn').on('click',login);
@@ -19,11 +24,33 @@ $(function () {
                     $.cookie('nickName',res.datas.nickName);
                     $.cookie('headImg',res.datas.headImg);
                     $('#login_display_reset').trigger('click');
+                    check_user_display();
                 }
                 else {
                     $('#login_msg').css('display','inline');
                 }
             }
-        })
+        });
+    }
+    
+    // 显示用户信息的函数
+    function check_user_display() {
+        if ($.cookie('email')==='null' || $.cookie('email')===undefined){
+            $('.login_undisplay_model').css('display','block');
+            $('.login_display_model').css('display','none');
+        }
+        else {
+            $('.login_undisplay_model').css('display','none');
+            $('.login_display_model').css('display','block');
+        }
+    }
+    
+    // 登出
+    function logout() {
+        $.cookie('email', null);
+        $.cookie('password',null);
+        $.cookie('nickName',null);
+        $.cookie('headImg',null);
+        check_user_display();
     }
 });
